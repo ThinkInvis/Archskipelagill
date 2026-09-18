3296c5b9ea2b2e6abf7885ed806559ee6b87dedf
ThinkInvis <thinkinvisible@protonmail.com>
2026-09-18 09:23:14 -0500

    Cleanup usings

--- UnityPlugin/SkillTree.cs
+++ UnityPlugin/SkillTree.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Linq;
 using UnityEngine;
 using System.IO;
-using HarmonyLib;
 
 namespace Archskipelagill;
 
--- UnityPlugin/SkillTreeItemizer.cs
+++ UnityPlugin/SkillTreeItemizer.cs
@@ -1,12 +1,7 @@
-﻿using Archskipelagill.Archipelago;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEngine.ParticleSystem.PlaybackState;
 
 namespace Archskipelagill;
 
