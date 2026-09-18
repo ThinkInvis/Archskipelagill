4ccdb2a5c439b9f22374e303f82c1096d2281fcd
ThinkInvis <thinkinvisible@protonmail.com>
2026-09-18 12:13:03 -0500

    Add support for save file relocation and wiping

---
+++ UnityPlugin/CustomSaveLoad.cs
@@ -0,0 +1,47 @@
+﻿using MonoMod.Cil;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Archskipelagill;
+
+public class CustomSaveLoad {
+    public CustomSaveLoad() {
+        IL.JSONsaver.save += JSONsaver_save;
+        IL.JSONsaver.load += JSONsaver_load;
+    }
+
+    private void JSONsaver_load(ILContext il) {
+        ILCursor c = new(il);
+        c.GotoNext(MoveType.After, x => x.MatchLdstr("/save.json"));
+        c.EmitDelegate<Func<string, string>>((origStr) => $"/archi-save-{Plugin.instance.cfgRunSuffix.Value}.json");
+        c.GotoNext(MoveType.After, x => x.MatchLdstr("/save.json"));
+        c.EmitDelegate<Func<string, string>>((origStr) => $"/archi-save-{Plugin.instance.cfgRunSuffix.Value}.json");
+    }
+
+    private void JSONsaver_save(ILContext il) {
+        ILCursor c = new(il);
+        c.GotoNext(MoveType.After, x => x.MatchLdstr("/save.json"));
+        c.EmitDelegate<Func<string, string>>((origStr) => $"/archi-save-{Plugin.instance.cfgRunSuffix.Value}.json");
+    }
+
+    public void Wipe() {
+        if(SceneManager.GetActiveScene().name != "menu") {
+            Plugin.BepinLogger.LogError("CustomSaveLoad.Wipe must be called from main menu");
+            return;
+        }
+        var saver = GameObject.FindGameObjectWithTag("MetaProg").GetComponent<JSONsaver>();
+        saver.metaProg = [];
+        var mb = GameObject.FindGameObjectWithTag("MoneyBag");
+#pragma warning disable IDE0031 //DO NOT use null propagation on Unity objects
+        if(mb != null) mb.GetComponent<MoneyBagScript>().addMoneyToBag();
+#pragma warning restore IDE0031
+
+        var chara = GameObject.Find("MetaGridAndStuff/PlayerCharacter").GetComponent<CharaStats>();
+        chara.XP = 0;
+        chara.XPtoDisplay = 0;
+
+        GameObject.FindFirstObjectByType<gridResetter>().resetMetaProg(); //also saves save file
+    }
+}
--- UnityPlugin/Plugin.cs
+++ UnityPlugin/Plugin.cs
@@ -2,8 +2,11 @@
 using Archskipelagill.Itemizers;
 using Archskipelagill.Utils;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
+using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Archskipelagill;
 
@@ -12,6 +15,10 @@ public class Plugin:BaseUnityPlugin {
     public const string PluginGUID = "com.ThinkInvisible.Archskipelagill";
     public const string PluginName = "Archskipelagill";
     public const string PluginVersion = "1.0.0";
+    public static Plugin instance { get; private set; }
+
+    public ConfigFile config { get; private set; }
+    public ConfigEntry<string> cfgRunSuffix;
 
     public const string ModDisplayInfo = $"{PluginName} v{PluginVersion}";
     private const string APDisplayInfo = $"Archipelago v{ArchipelagoClient.APVersion}";
@@ -20,17 +27,28 @@ public class Plugin:BaseUnityPlugin {
     public SkillTreeItemizer skillTreeItemizer;
     public RoundEndItemizer roundEndItemizer;
     public AbilityUnlockItemizer abilityUnlockItemizer;
+    public CustomSaveLoad customSaveLoad;
 
 #pragma warning disable IDE0051 //Used by Unity Engine
     private void Awake() {
+        if(instance != null) {
+            Logger.LogFatal("Duplicate plugin instance registered, this shouldn't be possible!");
+        }
+        instance = this;
+
         // Plugin startup logic
         BepinLogger = Logger;
+        config = new(Path.Combine(Paths.ConfigPath, PluginGUID + ".cfg"), true);
+
+        cfgRunSuffix = config.Bind<string>(new ConfigDefinition("Save/Load", "Run Suffix"), "default", new ConfigDescription("A suffix added to the custom save file redirect used by the client plugin. Must be changed if you want to participate in multiple Archipelago runs including this game simultaneously."));
+
         ArchipelagoClient = new ArchipelagoClient();
         ArchipelagoConsole.Awake();
 
         skillTreeItemizer = new();
         roundEndItemizer = new();
         abilityUnlockItemizer = new();
+        customSaveLoad = new();
 
         ArchipelagoConsole.LogMessage($"{ModDisplayInfo} loaded!");
     }
@@ -39,6 +57,17 @@ public class Plugin:BaseUnityPlugin {
         ArchipelagoClient.Disconnect();
     }
 
+    private void Update() {
+        if(confirmWipeSaveMode) {
+            confirmWipeSaveTimer += Time.deltaTime;
+            if(confirmWipeSaveTimer >= 20f) {
+                confirmWipeSaveMode = false;
+                confirmWipeSaveTimer = 0f;
+            }
+        }
+    }
+    float confirmWipeSaveTimer = 0f;
+    bool confirmWipeSaveMode = false;
     private void OnGUI() {
         
         GUI.BeginGroup(new Rect(Screen.width - 332, 200, 332, Screen.height - 200));
@@ -71,25 +100,50 @@ public class Plugin:BaseUnityPlugin {
                 ArchipelagoClient.Connect();
             }
         }
+
+        var wipeStr = "! Reset Save File !";
+        bool allowWipe = false;
+        if(confirmWipeSaveMode) {
+            if(confirmWipeSaveTimer < 5f) {
+                wipeStr = $"! Reset - ARE YOU SURE? Wait {(5f - confirmWipeSaveTimer):n0}s... !";
+            } else {
+                wipeStr = $"! Reset - Click again to confirm !";
+                allowWipe = true;
+            }
+        }
+        if(SceneManager.GetActiveScene().name == "menu") {
+            if(GUI.Button(new Rect(16, 180, 300, 20), wipeStr)) {
+                if(confirmWipeSaveMode) {
+                    if(allowWipe) {
+                        customSaveLoad.Wipe();
+                        confirmWipeSaveTimer = 0f;
+                        confirmWipeSaveMode = false;
+                    }
+                } else {
+                    confirmWipeSaveMode = true;
+                    confirmWipeSaveTimer = 0f;
+                }
+            }
+        }
         // a bunch of debug buttons
 #if DEBUG
-        if(GUI.Button(new Rect(16, 180, 200, 20), "DEBUG: Bake Skill Tree Map")) {
+        if(GUI.Button(new Rect(16, 210, 200, 20), "DEBUG: Bake Skill Tree Map")) {
             SkillTree.BuildAndExportMaps();
         }
-        if(GUI.Button(new Rect(16, 210, 200, 20), "DEBUG: Interrupt Connection")) {
+        if(GUI.Button(new Rect(16, 240, 200, 20), "DEBUG: Interrupt Connection")) {
             ArchipelagoClient.Disconnect();
         }
-        if(GUI.Button(new Rect(16, 240, 200, 20), "DEBUG: Cheat: Lodsemone")) {
+        if(GUI.Button(new Rect(16, 270, 200, 20), "DEBUG: Cheat: Lodsemone")) {
             var cs = GameObject.Find("PlayerCharacter").GetComponent<CharaStats>();
             cs.XP += 1000;
             cs.XPtoDisplay += 1000;
             cs.totalXP += 1000;
         }
-        if(GUI.Button(new Rect(16, 270, 200, 20), "DEBUG: Cheat: Regen")) {
+        if(GUI.Button(new Rect(16, 300, 200, 20), "DEBUG: Cheat: Regen")) {
             var cs = GameObject.Find("PlayerCharacter").GetComponent<CharaStats>();
             cs.HPREGEN += 10000;
         }
-        if(GUI.Button(new Rect(16, 300, 200, 20), "DEBUG: Cheat: Skip Time")) {
+        if(GUI.Button(new Rect(16, 330, 200, 20), "DEBUG: Cheat: Skip Time")) {
             var ts = GameObject.Find("Main Camera/Canvas/timer").GetComponent<timerScript>();
             ts.t = ts.endTime - 10f;
         }
