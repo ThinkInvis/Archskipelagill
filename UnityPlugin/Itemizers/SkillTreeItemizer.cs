e6c987824f430396054bb6173adb7d9bb4132745
ThinkInvis <thinkinvisible@protonmail.com>
2026-09-19 07:28:43 -0500

    Implement final boss key

--- UnityPlugin/Itemizers/SkillTreeItemizer.cs
+++ UnityPlugin/Itemizers/SkillTreeItemizer.cs
@@ -30,7 +30,10 @@ public class SkillTreeItemizer {
             tkr.sortedIndex = i;
             tkr.node = SkillTree.skillTree[i];
 
-            if(ArchiSaver.instance.receivedItemCounts.TryGetValue($"Skigill Region: {Enum.GetName(typeof(SkillTree.SkillNodeRegion), tkr.node.region).ToTitleCase()}", out var n) && n > 0)
+            var hasRegion = ArchiSaver.instance.receivedItemCounts.TryGetValue($"Skigill Region: {Enum.GetName(typeof(SkillTree.SkillNodeRegion), tkr.node.region).ToTitleCase()}", out var n) && n > 0;
+            var hasFBK = ArchiSaver.instance.receivedItemCounts.TryGetValue($"Final Boss Key", out n) && n > 0;
+
+            if(hasRegion && (tkr.node.type != SkillTree.SkillNodeType.BOSS_FINAL || hasFBK))
                 tkr.Unlock();
             else
                 tkr.Lock();
